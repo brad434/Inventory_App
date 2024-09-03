d0714ec82ea47b3b1b61927b7a86484dbd1b5645
Bradley Chery <bchery618@gmail.com>
2024-09-03 01:10:17 -0400

    edited index.html file and navbar

--- client/inventory-client/src/components/NavBar.jsx
+++ client/inventory-client/src/components/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = ({ isLoggedIn, isAdmin, handleLogout }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-black">
       <div className="container">
-        <Link className="navbar-brand" to="/">MyApp</Link>
+        <Link className="navbar-brand" to="/">Morrison Mentors</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
